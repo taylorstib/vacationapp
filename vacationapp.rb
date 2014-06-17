fadf37c2bd3a5cfeaa7daecb9d8744953ddc1bb5
Taylor Stib <tstib3@gmail.com>
2014-06-17 15:40:55 -0400

    Can post new items to the checklist

--- vacationapp.rb
+++ vacationapp.rb
@@ -5,17 +5,29 @@ configure do
 	enable :sessions
 end
 
+helpers do
+	def sess
+		session['todo']
+	end
+end
+
 get '/' do
-
 	erb :index
-
 end
 
 get '/checklist' do
-
-	
-
-	erb :checklist
-
+	params[:todo] ||= []
+	session[:user_todo] ||= []
+	array_todo = ['Take out the Trash before you leave.', 'Print boarding passes']
+	erb :checklist, :locals => {:array_todo => array_todo, :user_todo => session[:user_todo]}
 end
 
+
+post '/checklist' do
+	params[:todo] ||= []
+	session[:user_todo] ||= []
+	array_todo = ['Take out the Trash before you leave.', 'Print boarding passes']
+	new_todo = params[:todo]
+	session[:user_todo].push(new_todo)
+	erb :checklist, :locals => {:array_todo => array_todo, :user_todo => session[:user_todo]}
+end
