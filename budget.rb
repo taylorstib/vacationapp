b090e7d0d446e4e20be123c520026b4e45af7c62
Taylor Stib <tstib3@gmail.com>
2014-06-19 10:51:49 -0400

    Created zipped arrays for bills and splitters

--- budget.rb
+++ budget.rb
@@ -16,9 +16,31 @@ class Budget
 		remaining_budget = @totalbudget - @elements_total
 	end
 	
-	def bill_split
-		
+end
+
+class Bills
+	def initialize(transportation, transportation_split, lodging, lodging_split, amenities, amenities_split, dining, dining_split, other, other_split)
+		@transportation = transportation
+		@transportation_split = transportation_split
+		@lodging = lodging
+		@lodging_split = lodging_split
+		@amenities = amenities
+		@amenities_split = amenities_split
+		@dining = dining
+		@dining_split = dining_split
+		@other = other
+		@other_split = other_split
 	end
 
+	def splitting
+		array_bills = [@transportation, @lodging, @amenities, @dining, @other]
+		array_split = [@transportation_split, @lodging_split, @amenities_split, @dining_split, @other_split]
 
+		array_bills_split = array_bills.zip(array_split)
+
+		array_bills_split.each_with_index do |item, index|
+			item[index][0]/item[index][1]
+			
+		end
+	end
 end
